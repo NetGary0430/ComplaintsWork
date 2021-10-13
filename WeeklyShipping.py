af790eaeed6cfc7eaf3a69486f358a92f7354446
Gary Netherton <garynetherton@gmail.com>
2021-10-13 14:05:35 -0700

    Added Daily shipping complaints

--- WeeklyShipping.py
+++ WeeklyShipping.py
@@ -33,7 +33,7 @@ query = '''SELECT CD.[ComplaintNum],CD.[Status],CD.[SoNum]
   JOIN [NwdComplaints].[dbo].[ComplaintMast] AS M
   ON (CD.[ComplaintNum] = M.[ComplaintNum])
   WHERE CD.[Status] IN ('Active', 'Closed') AND
-        CD.[ComplaintNum] > 50000 AND
+        M.[RespDept] IN ('SHIPWH') AND
 		CD.[OpenDate] >=DATEADD(day, DATEDIFF(day,1,GETDATE()),0) 
         AND CD.[OpenDate] < DATEADD(day, DATEDIFF(day,0,GETDATE()),0);'''
 df = pd.read_sql(query, cnxn)
@@ -51,33 +51,6 @@ tbl_Out = HTML(df.to_html(classes='table table-striped'))
 dfPrint = df[['Dept', 'ComplaintNum', 'SoNum', 'SOItem', 'Product', 'PartCost', 'RC_Description', 'AffQty', 'ComplaintCost']]
 dfPrint.sort_values(by=['Dept'], inplace=True)
 
-""" ####################################################################################################
-from reportlab.platypus import Paragraph, Spacer, Table, Image, SimpleDocTemplate
-from reportlab.lib.styles import (ParagraphStyle, getSampleStyleSheet)
-import os, sys
-from reportlab.pdfgen import canvas
-
-
-def generate_report(attachment, title, paragraph):
-    report = SimpleDocTemplate(attachment)
-
-    print("--------------------------------")
-    print(str(dfPrint))
-    print("--------------------------------")
-
-    flowables = []
-
-    styles = getSampleStyleSheet()
-
-
-    report_title = Paragraph(title, styles["h1"])
-    paragraph1 = Paragraph(paragraph, styles["h5"])
-
-    flowables.append(report_title)
-    flowables.append(paragraph1)
-
-    report.build(flowables)
-#################################################################################################### """
 ####################################################################################################
 ####################################################################################################
 
@@ -86,7 +59,8 @@ if __name__ == "__main__":
     #qcReport = generate_report('C:/temp/complaintsdept.pdf', "Complaints Report generated " + datetime.datetime.now().strftime("%B %d, %Y"), (dfPrint.to_string()))
     outlook = win32.Dispatch('outlook.application')
     mail = outlook.CreateItem(0)
-    mail.To = 'gnetherton@northwestdoor.com; mmartin@northwestdoor.com; sjones@northwestdoor.com; jfrench@northwestdoor.com; choffman@northwestdoor.com; wbaer@northwestdoor.com'
+    # mail.To = 'gnetherton@northwestdoor.com; mmartin@northwestdoor.com; sjones@northwestdoor.com; jfrench@northwestdoor.com; choffman@northwestdoor.com; wbaer@northwestdoor.com'
+    mail.To = 'gnetherton@northwestdoor.com; choffman@northwestdoor.com'
     mail.Subject = 'Complaint Summary '+ datetime.datetime.now().strftime("%B %d, %Y")
     mail.Body = '''Please find data attached and below.\n\n
                {}'''.format(dfPrint.to_string())
